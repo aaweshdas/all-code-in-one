c5b21798ec0582c5bdb2c88a241b5b443382b4d4
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-09-04 12:17:32 +0545

    Code

--- 7thprac.c
+++ 7thprac.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    
+    int a,b,c;
+    printf("Please enter the value a = %d\n");
+    scanf("%d",&a);
+    printf("Please enter the value b = %d\n");
+    scanf("%d",&b);
+    printf("\nbefore swping value =%d , b=%d", a,b);
+    a=c;
+    b=a;
+    c=b;
+    printf("\nafter swping value =%d , b=%d", a,b);
+    return 0;
 }
---
+++ SI.c
@@ -0,0 +1,21 @@
+#include <Stdio.h>
+int main()
+{
+    int p,t,r;
+     printf("Please enter the Value of p and t and r = ");
+     scanf("%d", &p , &t ,&r);
+     printf("Simple Interest %d", S=(P*t*r)/100);
+     return 0;
+}
+/*
+#include <stdio.h>
+
+int main() {
+    int p, t, r;
+    printf("Please enter the values of p, t, and r: ");
+    scanf("%d %d %d", &p, &t, &r);
+    int S = (p * t * r) / 100;
+    printf("Simple Interest: %d\n", S);
+    return 0;
+}
+*/
