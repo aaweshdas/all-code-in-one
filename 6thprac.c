1b03746057b2994bb55af3a194036016bdf5efd6
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-09-04 11:43:03 +0545

    Code

---
+++ 6thprac.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("Add = %d\n", a+b);
+    printf("sub = %d\n", a-b);
+    printf("multi = %d\n", a*b);
+    printf("div = %d\n", a/b);
+    return 0;
+    
+
+}
