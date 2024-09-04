028bf1ce133a69109a43b780d16471b186aa39c8
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-09-04 08:48:41 +0545

    Code

---
+++ Largerstvalue.c
@@ -0,0 +1,14 @@
+// Largest value //
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter a value:");
+    scanf("%d",&a);
+    printf("enter b value;");
+    scanf("%d",&b);
+    c = (a+b)/2-(a-b)/2;
+    printf("largest value is = %d",c);
+
+}
