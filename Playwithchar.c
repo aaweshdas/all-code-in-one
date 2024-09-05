ee190b477730dbc45948c558e9ad9f54e70392e6
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-09-05 18:26:23 +0545

    Code

---
+++ Playwithchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+  char a;
+  char b[10];
+  char sentence[200];
+  
+  
+  scanf("%c",&a);
+  getchar();
+
+  scanf("%s",b);
+  getchar();
+  fgets(sentence,200,stdin);
+  
+  printf("%c",a);
+  
+  printf("\n%s",b);
+   printf("\n%s",sentence);
+    return 0;
+}
