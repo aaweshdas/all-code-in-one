f0e435484d1655bb647ec0778fab130863fcf886
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-09-11 16:26:06 +0545

    Code

--- practice.c
+++ practice.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main()
 {
@@ -8,7 +8,24 @@ int main()
     printf("\n%d",a);
      a-=5;
     printf("\n%d",a);
+    a*=5;
+    printf("\n%d",a);
     a++;
+    printf("\n%d",a);
+    a--;
+    printf("\n%d",a);
+}*/
+#include <stdio.h>
+
+int main()
+{
+     int a=7;
+    printf("%d",a);
+    a+=10;
+    printf("\n%d",a);
+     a*=11;
+    printf("\n%d",a);
+     a++;
     printf("\n%d",a);
     a--;
     printf("\n%d",a);
