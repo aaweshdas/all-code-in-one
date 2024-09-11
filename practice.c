98ddfd6df34d0156d14abb3649a8b9e2c8a066a8
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-09-11 16:09:22 +0545

    Code

--- practice.c
+++ practice.c
@@ -2,5 +2,14 @@
 
 int main()
 {
-     
+     int a=5;
+    printf("%d",a);
+    a+=5;
+    printf("\n%d",a);
+     a-=5;
+    printf("\n%d",a);
+    a++;
+    printf("\n%d",a);
+    a--;
+    printf("\n%d",a);
 }
