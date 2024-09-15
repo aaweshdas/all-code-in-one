eb4e2a1ed7f0807c01f8251a7d0388bafd0b76aa
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-09-15 10:22:11 +0545

    Code

---
+++ C_to_F.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float c, f;
+    printf("Enter the Centigrade value:" );
+    scanf("%f",&c );
+
+    f = (c * 9/5) + 32 ;
+    printf("Fahrenheit %f" ,f);
+    return 0;
+}
+//  °C × (9/5) + 32
