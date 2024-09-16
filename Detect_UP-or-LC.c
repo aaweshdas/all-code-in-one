156c47e6001b20a5f12faba0520e09598eb6398a
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-09-16 23:00:50 +0545

    Code

---
+++ Detect_UP-or-LC.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    // Input a character
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    // Check if uppercase
+    if (ch >= 'A' && ch <= 'Z') {
+        printf("The character '%c' is an uppercase letter.\n", ch);
+    }
+    // Check if lowercase
+    else if (ch >= 'a' && ch <= 'z') {
+        printf("The character '%c' is a lowercase letter.\n", ch);
+    }
+    // If it's neither uppercase nor lowercase
+    else {
+        printf("The character '%c' is not a letter.\n", ch);
+    }
+
+    return 0;
+}
