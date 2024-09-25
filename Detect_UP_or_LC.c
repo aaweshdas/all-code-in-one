b49283cb27f3d994f9d60f935933d894dc2b0070
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-09-25 14:28:53 +0530

    Code

--- Detect_UP_or_LC.c
+++ Detect_UP_or_LC.c
@@ -1,6 +1,6 @@
 // Analyse the uppercase or lower case
 
-#include <stdio.h>Q
+#include <stdio.h>
 int main() 
 {
     char ch;
@@ -20,5 +20,5 @@ int main()
     else {
         printf("The character '%c' is not a letter.\n", ch);
     }
-    return 0;  
-}             
+    return 0;
+}
