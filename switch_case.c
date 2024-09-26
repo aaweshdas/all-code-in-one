573ec9d5d21189ab3f5dd166b4648cf28050cbf8
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-09-26 10:15:05 +0530

    Code

---
+++ switch_case.c
@@ -0,0 +1,32 @@
+//Program for Switch case 
+
+#include<stdio.h>
+int main()
+{
+    int n1,n2;
+    char a;
+    printf("Enter the first number: ");
+    scanf("%d",&n1);
+    printf("Enter the second number: ");
+    scanf("%d",&n2);
+    printf("Enter the operator (+,-,*,/): ");
+    scanf("\n%c",&a);
+    switch(a)
+    {
+        case '+':
+        printf("%d + %d = %d",n1,n2,n1+n2);
+        break;
+        case '-':
+        printf("%d - %d = %d",n1,n2,n1-n2);
+        break;
+        case '*':
+        printf("%d * %d = %d",n1,n2,n1*n2);
+        break;
+        case '/':
+        printf("%d / %d = %d",n1,n2,n1/n2);
+        break;
+        defult:
+        printf("Error! Please Enter the Correct Operater , Ha HA ha");
+    }
+    return 0;
+}
