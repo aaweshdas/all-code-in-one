023ee56c9f2188f57c96c425cc3978158b737cd4
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-09-30 15:20:59 +0530

    Code

---
+++ loops.c
@@ -0,0 +1,25 @@
+// Write a program using a for Loop condition.....
+// #include <stdio.h>
+// int main()
+// {
+//     int i, n;
+//     printf("enter the i &n value: ");
+//     scanf("%d", &n);
+//     for(i=0;i<n;i++)
+//     {
+//         printf("%d",i);
+//     }
+// }
+
+#include <stdio.h>
+int main()
+{
+    int i, n;
+    printf("enter the i &n value: ");
+    scanf("%d", &n);
+    for(i=0;i<n;i++)
+    {
+        printf("%d",i);
+    }
+}
+
