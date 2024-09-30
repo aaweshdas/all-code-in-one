1078e5edea1839923a1879f2cb1c86a1b3309356
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-09-30 16:00:17 +0530

    Code

---
+++ loop2.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int i;
+//     for(i=0;i<5;i++)
+//     {
+//         printf("*");
+//     }
+// }
+
+#include <stdio.h>
+int main()
+{
+    int i;
+    for(i=0;i<1;i++)
+    {
+        printf("Aawesh Kumar Das\n I am Owner of my own world\n");
+    }
+}
