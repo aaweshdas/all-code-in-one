d197b88876adac0195ac3da5789cc654f3ff165d
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-10-06 00:40:36 +0530

    Code

---
+++ new.c
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int findSmallestPositiveInteger(int arr[], int n) {
+    int res = 1;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] <= res) {
+            res += arr[i];
+        } else {
+            break;
+        }
+    }
+    return res;
+} 
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int result = findSmallestPositiveInteger(arr, n);
+    cout << "The smallest positive integer that cannot be represented as the sum of elements of any subset of the given array is: " << result << endl;
+
+    return 0;
+}
