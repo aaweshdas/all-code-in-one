a3d7421d89bd0eab26084f1e75b1adc88e907c62
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-10-06 00:40:54 +0530

    Code

--- new.c
+++ new.c
@@ -18,7 +18,7 @@ int main() {
     cout << "Enter the size of the array: ";
     cin >> n;
 
-    int arr[n];
+    int arr[n]; 
     cout << "Enter the elements of the array: ";
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
