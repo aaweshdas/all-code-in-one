a64b2c8a70c8e1a49c77e03a49839c6aa2f27c29
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-10-09 23:39:47 +0530

    Code

---
+++ Palindrome_or_not.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 100
+
+int main() {
+    char str[MAX_LEN];
+    int len, i, isPalindrome = 1; // Assume it is a palindrome
+
+    // Input the string from the user
+    printf("Enter a string: ");
+    fgets(str, MAX_LEN, stdin);
+    
+    // Remove the newline character if present
+    str[strcspn(str, "\n")] = 0; 
+
+    len = strlen(str);
+
+    // Check for palindrome
+    for (i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - i - 1]) {
+            isPalindrome = 0; // Not a palindrome
+            break;
+        }
+    }
+ 
+    // Output the result
+    if (isPalindrome) {
+        printf("\"%s\" is a palindrome.\n", str);
+    } else {
+        printf("\"%s\" is not a palindrome.\n", str);
+    }
+
+    return 0;
+}
