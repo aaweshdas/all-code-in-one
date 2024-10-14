1a8dee2324131a796d10715804332742f8ad8dab
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-10-14 15:10:20 +0530

    Code

--- while_loop.c
+++ while_loop.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-int main()
-{
-    int i=3;
-    while(i<=5)
-    {
-        printf("Given number is positive");
-       return 0;
+// #include <stdio.h>
+// int main()
+// {
+//     int i=3;
+//     while(i<=5)
+//     {
+//         printf("Given number is positive");
+//        return 0;
 
-    }
-    i++;
-} 
+//     }
+//     i++;
+// } 
