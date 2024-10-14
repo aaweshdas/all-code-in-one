0b21dabcf46cd0723cef741ea289fca7a32e1b07
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-10-14 15:24:42 +0530

    Code

---
+++ while_loop_infinite.c
@@ -0,0 +1,29 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int g1 = 1;
+//     int g2 = 2;
+//     while (g1 < 10)
+//     {
+//         g2 = g2 + 1;
+//         printf("infinity\n");
+
+//     } 
+//     return 0;
+// }
+
+#include <stdio.h>
+int main()
+{
+    int g1 = 1;
+    int g2 = 2;
+    while (g1 < 10)
+    {
+        g2 = g2 + 1;
+        printf("infinity\n");
+        printf("%d g2 value\n");
+        break;
+        
+    } 
+    return 0;
+}
