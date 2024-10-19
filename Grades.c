e47a84e9fa455cb5b87b997fcca780a80d5421d5
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-10-19 21:49:22 +0530

    Code

--- Grades.c
+++ Grades.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include<math.h>   
      
-int main() {   
+int main() {     
     int marks;
     printf("Enter your marks (0-100): ");
     scanf("%d", &marks);
@@ -9,10 +9,10 @@ int main() {
     if (marks >= 90) {
         printf("Grade: O\n");
     } else if (marks >= 80) { 
-        printf("Grade: A\n");
+        printf("Grade: A\n"); 
     } else if (marks >= 70) {
         printf("Grade: B\n"); 
-    } else if (marks >= 60) {
+    } else if (marks >= 60) { 
         printf("Grade: C\n");
     } else
  {
