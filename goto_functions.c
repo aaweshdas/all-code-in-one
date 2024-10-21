f0d6449bd09ae8e355efedcf6c381013ca28eca6
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-10-21 21:41:59 +0530

    Code

---
+++ goto_functions.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main() {
+    int i = 0;
+    printf("Starting loop...\n");
+
+    // Label for the loop
+    loop:
+    printf("Iteration %d\n", i);
+
+    // Increment i
+    i++;
+
+    // Check if i is less than 5
+    if (i < 5) {
+        // If true, jump to the label 'loop'
+        goto loop;
+    }
+
+    printf("Loop ended.\n");
+    return 0;
+}
