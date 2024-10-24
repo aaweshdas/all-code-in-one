adf028d903ac0e180121dce09407f59e339f8443
Aawesh <140650745+Aaweshdas@users.noreply.github.com>
2024-10-24 23:35:21 +0530

    Code

--- switch_case.c
+++ switch_case.c
@@ -6,7 +6,7 @@ int main()
     int n1,n2;        
     char a;  
     printf("Enter the first number: ");
-    scanf("%d",&n1);
+    scanf("%d",&n1); 
     printf("Enter the second number: ");
     scanf("%d",&n2);
     printf("Enter the operator (+,-,*,/): ");
