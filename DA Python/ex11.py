4a2f56a0227a6a7714daf6eadcc4b3ac67f62cac
aaweshdas <aaravdas208@gmail.com>
2025-08-06 13:44:02 +0530

    Code

--- DA Python/ex11.py
+++ DA Python/ex11.py
@@ -10,7 +10,7 @@
 # marks = list(map(int, input("Enter 5 marks: ").split()))
 # print("Average:", average_marks(marks))
 
-# #Create a function that checks if the given year is a leap year.
+# #Create a function that checks if the given year is a leap year. 
 # def is_leap(year):
 #     return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
 # year = int(input("Enter year: "))
